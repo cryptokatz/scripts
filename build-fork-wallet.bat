59d8259f28a45e7bccfa5a6e87ffa323f8a7247b
darkharvest <kahthan@gmail.com>
2018-07-31 09:14:50 +0530

    update

--- build-fork-wallet.bat
+++ build-fork-wallet.bat
@@ -1,13 +1,13 @@
 @call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsMSBuildCmd.bat"
-cd "c:\working\fork\conceal-classic"
+cd "c:\working\fork\conceal-wallet"
 del CMakeLists.txt
 copy C:\Working\dev\CMakeLists.txt  /Y
 mkdir cryptonote
-xcopy C:\Working\fork\conceal-core c:\Working\fork\conceal-classic\cryptonote /E /y
+xcopy C:\Working\conceal-core c:\Working\fork\conceal-wallet\cryptonote /E /y
 mkdir build
 cd build
-xcopy C:\Working\dev\gui\*.dll c:\Working\fork\conceal-classic\build\Release /E /y
+xcopy C:\Working\dev\gui\*.dll c:\Working\fork\conceal-wallet\build\Release /E /y
 cmake -G "Visual Studio 15 2017 Win64" -DBOOST_LIBRARYDIR:PATH=c:/local/boost_1_67_0 ..
-msbuild conceal-classic.sln /p:Configuration=Release /m
+msbuild conceal-gui.sln /p:Configuration=Release /m
 cd Release
 dir 
