c4f82fc9f4b770f6bddbeaa081fd29e197208352
darkharvest <kahthan@gmail.com>
2018-10-18 11:12:08 +0530

    testnet gui on mainnet

---
+++ testnet-wallet2.bat
@@ -0,0 +1,14 @@
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsMSBuildCmd.bat"
+cd "c:\working\testnet\testnet-wallet"
+del CMakeLists.txt
+copy C:\Working\dev\CMakeLists.txt  /Y
+mkdir cryptonote
+xcopy c:\working\conceal-core c:\working\testnet\testnet-wallet\cryptonote /E /y
+cd "c:\working\testnet\testnet-wallet"
+mkdir build
+cd build
+xcopy C:\Working\dev\gui\*.dll c:\working\testnet\testnet-wallet\build\Release /E /y
+cmake -G "Visual Studio 15 2017 Win64" -DBOOST_LIBRARYDIR:PATH=c:/local/boost_1_67_0 ..
+msbuild testnet-gui.sln /p:Configuration=Release /m
+cd Release
+dir 
