4f658b55fd21104e2f6fd1879b0e2ca9d9e7cb4e
katz <cryptokatz1@protonmail.com>
2020-01-27 12:52:26 +0530

    Update build-wallet.bat

--- build-wallet.bat
+++ build-wallet.bat
@@ -1,12 +1,12 @@
 @call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsMSBuildCmd.bat"
-cd "c:\working\conceal-wallet"
+cd "c:\working\conceal-desktop"
 del CMakeLists.txt
 copy C:\Working\dev\CMakeLists.txt  /Y
 mkdir cryptonote
-xcopy C:\Working\conceal-core c:\Working\conceal-wallet\cryptonote /E /y
+xcopy C:\Working\conceal-core c:\Working\conceal-desktop\cryptonote /E /y
 mkdir build
 cd build
-xcopy C:\Working\dev\gui\*.dll c:\Working\conceal-wallet\build\Release /E /y
+xcopy C:\Working\dev\gui\*.dll c:\Working\conceal-desktop\build\Release /E /y
 cmake -G "Visual Studio 15 2017 Win64" -DBOOST_LIBRARYDIR:PATH=c:/local/boost_1_67_0 ..
 msbuild conceal-gui.sln /p:Configuration=Release 
 cd Release
